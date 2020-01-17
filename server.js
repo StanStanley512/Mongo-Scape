63434fab87065ee0207c664b980a4d0f2618833b
StanStanley512 <shane.stanleym@gmail.com>
2020-01-16 19:45:13 -0600

    updated server.js mongoduri

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ var PORT = process.env.PORT || 8000;
 var app = express();
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
 mongoose.connect(MONGODB_URI);
 
 // Use morgan logger for logging requests
